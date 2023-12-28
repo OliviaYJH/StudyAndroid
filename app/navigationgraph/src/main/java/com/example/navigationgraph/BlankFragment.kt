8f07dad2e7ba3808e0eb9a1e754fed6ffc73e9f3
OliviaYJH <0703olivia@naver.com>
2023-12-28 16:46:32 +0900

    jetpack navigation 적용하기

---
+++ app/navigationgraph/src/main/java/com/example/navigationgraph/BlankFragment.kt
@@ -0,0 +1,38 @@
+package com.example.navigationgraph
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.navigationgraph.databinding.FragmentBlankBinding
+
+class BlankFragment : Fragment() {
+    private var _binding: FragmentBlankBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentBlankBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.firstButton.setOnClickListener {
+            findNavController().navigate(R.id.blankFragment02)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "StudyAndroid"
 include(":app")
+include(":app:navigationgraph")
