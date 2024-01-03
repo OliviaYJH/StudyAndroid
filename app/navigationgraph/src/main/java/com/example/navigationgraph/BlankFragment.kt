47faa72585685494242aa5cafb225c2b9ecf72ad
OliviaYJH <0703olivia@naver.com>
2024-01-03 16:38:56 +0900

    safe args에서 default value 값 추가

--- app/navigationgraph/src/main/java/com/example/navigationgraph/BlankFragment.kt
+++ app/navigationgraph/src/main/java/com/example/navigationgraph/BlankFragment.kt
@@ -26,7 +26,9 @@ class BlankFragment : Fragment() {
 
         binding.firstButton.setOnClickListener {
             // 목적지가 아닌 액션을 매개변수로 전달
-            findNavController().navigate(BlankFragmentDirections.nextAction(30.toString()))
+            findNavController().navigate(BlankFragmentDirections.nextAction().apply {
+                blankArgNumber = "hello"
+            })
         }
     }
 
