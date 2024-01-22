0459641e5a8bae11676532bdb5d085b6f35f1d3d
YuJungHyun <0703olivia@naver.com>
2024-01-22 14:12:15 +0900

    count up, count down

---
+++ app/stopwatch/src/main/java/com/example/stopwatch/StopWatchActivity.kt
@@ -0,0 +1,171 @@
+package com.example.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import com.example.stopwatch.databinding.ActivityStopWatchBinding
+import com.example.stopwatch.databinding.DialogCountdownSettingBinding
+import java.util.Timer
+import kotlin.concurrent.timer
+
+class StopWatchActivity : AppCompatActivity() {
+    private var _binding: ActivityStopWatchBinding? = null
+    private val binding get() = _binding!!
+
+    private var countDownSecond = 10
+    private var currentCountdownDeciSecond = countDownSecond * 10
+
+    private var currentDeciSecond = 0
+    private var timer: Timer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = ActivityStopWatchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvCountDown.setOnClickListener {
+            showCountDownSettingDialog()
+        }
+
+        binding.btnStart.setOnClickListener {
+            start()
+
+            binding.btnStart.isVisible = false
+            binding.btnStop.isVisible = false
+            binding.btnPause.isVisible = true
+            binding.btnLap.isVisible = true
+        }
+
+        binding.btnStop.setOnClickListener {
+            showAlertDialog()
+        }
+
+        binding.btnPause.setOnClickListener {
+            pause()
+
+            binding.btnStart.isVisible = true
+            binding.btnStop.isVisible = true
+            binding.btnPause.isVisible = false
+            binding.btnLap.isVisible = false
+        }
+
+        binding.btnLap.setOnClickListener {
+            lap()
+
+            binding.btnStart.isVisible = false
+            binding.btnStop.isVisible = false
+            binding.btnPause.isVisible = true
+            binding.btnLap.isVisible = true
+        }
+
+        initCountdownViews()
+    }
+
+    private fun initCountdownViews() {
+        binding.tvCountDown.text = String.format("%02d", countDownSecond)
+        binding.pbCountDown.progress = 100 // 맨 처음은 100%
+    }
+
+    private fun start() {
+        // 1초 = 1000ms -> 이 앱에서는 0.1초마다 check == 100ms
+        // timer => Worker Thread가 생긴 것임
+        timer = timer(initialDelay = 0, period = 100) {
+            if (currentCountdownDeciSecond == 0) {
+                // count up
+                currentDeciSecond += 1 // 0.1초씩 증가
+                Log.d("currentDeciSecond", currentDeciSecond.toString())
+
+                val minutes = currentDeciSecond.div(10) / 60
+                val second = currentDeciSecond.div(10) % 60
+                val deciSeconds = currentDeciSecond % 10
+
+                // runOnUiThread 방법
+                runOnUiThread {// Activity 안에 있는 메서드
+                    // UI를 수정하는 작업
+                    binding.tvTime.text = String.format("%02d:%02d", minutes, second)
+                    binding.tvTick.text = deciSeconds.toString()
+
+                    // countdown 관련 UI 요소 안보이게
+                    binding.groupCountdown.isVisible = false
+                }
+            } else {
+                // count down
+                currentCountdownDeciSecond -= 1
+                val seconds = currentCountdownDeciSecond / 10
+                val progress = currentCountdownDeciSecond / (countDownSecond * 10f) * 100 // 백분율 float 형
+
+                // View에서 post하는 방법
+                binding.root.post {
+                    // UI를 수정하는 작업
+                    binding.tvCountDown.text = String.format("%02d", seconds)
+                    binding.pbCountDown.progress = progress.toInt()
+                }
+
+            }
+        }
+    }
+
+    private fun stop() {
+        binding.btnStart.isVisible = true
+        binding.btnStop.isVisible = true
+        binding.btnPause.isVisible = false
+        binding.btnLap.isVisible = false
+
+        // 초기화
+        currentDeciSecond = 0 // data 변경
+        // UI 변경
+        binding.tvTime.text = "00:00"
+        binding.tvTick.text = "0"
+
+        binding.groupCountdown.isVisible = true
+        initCountdownViews()
+    }
+
+    private fun pause() {
+        timer?.cancel()
+        timer = null
+    }
+
+    private fun lap() {
+
+    }
+
+    private fun showCountDownSettingDialog() {
+        AlertDialog.Builder(this).apply {
+            // dialog 띄우기
+            val settingBinding = DialogCountdownSettingBinding.inflate(layoutInflater)
+            // 기본 설정
+            with(settingBinding.npCountdownSecond) {
+                maxValue = 20
+                minValue = 0
+                value = countDownSecond // default 값
+            }
+            setTitle("카운트다운 설정")
+            setView(settingBinding.root)
+
+            // 나머지 설정
+            setPositiveButton("확인") { dialog, id ->
+                countDownSecond = settingBinding.npCountdownSecond.value
+                currentCountdownDeciSecond = countDownSecond * 10
+
+                binding.tvCountDown.text = String.format("%02d", countDownSecond)
+            }
+            setNegativeButton("취소", null)
+        }.show()
+    }
+
+    private fun showAlertDialog() {
+        AlertDialog.Builder(this).apply {
+            //setTitle()
+            setMessage("종료하시겠습니까?")
+            setPositiveButton("네") { dialog, id ->
+                stop()
+            }
+            setNegativeButton("아니오", null)
+        }.show()
+    }
+
+}
