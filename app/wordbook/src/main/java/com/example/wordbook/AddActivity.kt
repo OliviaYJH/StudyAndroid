2ceb3e3d0d06317044ff38fa9fd8b89f8c3d1848
OliviaYJH <0703olivia@naver.com>
2024-01-24 11:45:44 +0900

    WordBook - Chip 생성

--- app/wordbook/src/main/java/com/example/wordbook/AddActivity.kt
+++ app/wordbook/src/main/java/com/example/wordbook/AddActivity.kt
@@ -18,5 +18,23 @@ class AddActivity : AppCompatActivity() {
         initViews()
     }
 
-    
+    // command + shift + v => 그동안 clipboard 복사한 것 확인 가능
+    private fun initViews() {
+        val types = listOf("명사", "동사", "대명사", "형용사", "부사", "감탄사", "전치사", "접속사")
+
+        binding.chipGroupType.apply {
+            types.forEach { type ->
+                addView(createChip(type))
+            }
+        }
+    }
+
+    // chip 만들기
+    private fun createChip(type: String): Chip {
+        return Chip(this).apply {
+            text = type
+            isCheckable = true
+            isClickable = true
+        }
+    }
 }
