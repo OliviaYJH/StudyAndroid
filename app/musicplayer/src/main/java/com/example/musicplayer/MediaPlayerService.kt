2fbda0bb0ac77269d186e643d7eca16d3f73dd92
OliviaYJH <0703olivia@naver.com>
2024-02-06 15:50:48 +0900

    MediaPlayer - startForeground 사용해 Notification 띄우기

--- app/musicplayer/src/main/java/com/example/musicplayer/MediaPlayerService.kt
+++ app/musicplayer/src/main/java/com/example/musicplayer/MediaPlayerService.kt
@@ -1,7 +1,8 @@
 package com.example.musicplayer
 
-import android.app.Service
+import android.app.*
 import android.content.Intent
+import android.graphics.drawable.Icon
 import android.media.MediaPlayer
 import android.os.IBinder
 
@@ -10,6 +11,95 @@ class MediaPlayerService : Service() {
 
     override fun onBind(intent: Intent): IBinder? {
         return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        createNotificationChannel()
+
+        // 화면에 사용할 아이콘들
+        val playIcon = Icon.createWithResource(baseContext, R.drawable.ic_play)
+        val stopIcon = Icon.createWithResource(baseContext, R.drawable.ic_stop)
+        val pauseIcon = Icon.createWithResource(baseContext, R.drawable.ic_pause)
+
+        val mainPendingIntent = PendingIntent.getActivity(
+            baseContext,
+            0,
+            Intent(baseContext, MainActivity::class.java)
+                .apply {
+                       flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                       // 기존의 생성된 activity들이 있다면 기존읙 것들을 재사용함
+                },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val pausePendingIntent = PendingIntent.getService(
+            baseContext,
+            0,
+            Intent(baseContext, MediaPlayerService::class.java).apply { action = MEDIA_PLAYER_PAUSE },
+            PendingIntent.FLAG_IMMUTABLE // flag 넣기 - 더 이상 변경하지 않겠다
+        )
+        val playPendingIntent = PendingIntent.getService(
+            baseContext,
+            0,
+            Intent(baseContext, MediaPlayerService::class.java).apply { action = MEDIA_PLAYER_PLAY },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val stopPendingIntent = PendingIntent.getService(
+            baseContext,
+            0,
+            Intent(baseContext, MediaPlayerService::class.java).apply { action = MEDIA_PLAYER_STOP },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+
+        // Notification 생성
+        val notification = Notification.Builder(baseContext, CHANNEL_ID)
+            // 관련 내용 설정
+            .setStyle(
+                Notification.MediaStyle()
+                    .setShowActionsInCompactView(0, 1, 2) // 각각에 맞는 action이 들어가게 됨
+            )
+            .setVisibility(Notification.VISIBILITY_PUBLIC)
+            .setSmallIcon(R.drawable.ic_star)
+            .addAction(
+                Notification.Action.Builder(
+                    // 일시정지
+                    pauseIcon,
+                    "Pause", // title
+                    pausePendingIntent
+                ).build()
+            )
+            .addAction(
+                Notification.Action.Builder(
+                    playIcon,
+                    "Play",
+                    playPendingIntent
+                ).build()
+            )
+            .addAction(
+                Notification.Action.Builder(
+                    stopIcon,
+                    "Stop",
+                    stopPendingIntent
+                ).build()
+            )
+            .setContentIntent(mainPendingIntent) // Notification 자체를 클릭했을 때의 실행
+            .setContentTitle("음악 재생")
+            .setContentText("음원이 재생 중 입니다..")
+            .build()
+
+        
+        startForeground(100, notification)
+    }
+
+    // Notification channel 생성
+    private fun createNotificationChannel() {
+        val channelId = CHANNEL_ID
+        val channel = NotificationChannel(channelId, "MEDIA_PLAYER", NotificationManager.IMPORTANCE_DEFAULT)
+
+        val notificationManager = baseContext.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
