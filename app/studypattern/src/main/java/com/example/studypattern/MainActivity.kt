7184e47eb407ea3296a0cdffd2bf70f00b04fae3
YuJungHyun <0703olivia@naver.com>
2024-03-04 00:12:43 +0900

    StudyPattern - MainActivity setting
    - databinding 설정

--- app/studypattern/src/main/java/com/example/studypattern/MainActivity.kt
+++ app/studypattern/src/main/java/com/example/studypattern/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.studypattern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.studypattern.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // dataBinding 사용
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+            it.view = this
+        }
+    }
+
+    fun openMVC() {
+
+    }
+
+    fun openMVP() {
+
+    }
+
+    fun openMVVM() {
+
+    }
+
+    fun openMVI() {
+
     }
 }
